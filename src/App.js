2bf08fd63c2c934776cae7aec1dbbb78d1f457f3
Ashit Kumar Verma <39930106+vermatron@users.noreply.github.com>
2022-12-30 00:18:22 +0000

    Create basic react components (#1)
    * Created basic react component with css
    
    * Added nested components and basic states test
    
    * Added missing files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import Expense from "./components/Expense";
+
 function App() {
+  const expenseItems = [
+    {
+      id: "id1",
+      title: "car insurance",
+      amount: 450.0,
+      date: new Date(2022, 3, 23),
+    },
+    {
+      id: "id2",
+      title: "Home insurance",
+      amount: 4520.0,
+      date: new Date(2022, 3, 23),
+    },
+    {
+      id: "id1",
+      title: "Health insurance",
+      amount: 4150.0,
+      date: new Date(2022, 3, 23),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expense items={expenseItems} />
     </div>
   );
 }
